53e0afbf2222526aa4801de0c189aa7473ce79c8
AKouta01 <abdelwahabkouta01@gmail.com>
2023-09-14 20:10:11 +0200

    New Folder for Login and Signup

---
+++ lib/LogIn/login.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    child:
+    Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[],
+        ),
+      ),
+    );
+    throw UnimplementedError();
+  }
+}
---
+++ lib/SignUp/signup.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class SignupPage extends StatefulWidget {
+  const SignupPage({Key? key}) : super(key: key);
+
+  @override
+  State<SignupPage> createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+  @override
+  Widget build(BuildContext context) {
+    child:
+    Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[],
+        ),
+      ),
+    );
+    throw UnimplementedError();
+  }
+}
